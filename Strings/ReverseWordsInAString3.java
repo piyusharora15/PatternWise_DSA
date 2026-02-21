1b0a4a3e56a0d7480b2c955e23c1a9c5eb37cdf9
Piyush Arora <piyusharora1504@gmail.com>
2026-02-21 17:27:31 +0530

    Reverse Words in a String 3

--- Strings/ReverseWordsInAString3.java
+++ Strings/ReverseWordsInAString3.java
@@ -31,3 +31,30 @@ class Solution {
 
 // Time Complexity: O(n), where n is the length of the string.
 // Space Complexity: O(n), since we are using a character array to store the string.
+
+// https://www.geeksforgeeks.org/problems/reverse-each-word-in-a-given-string1001/1
+
+// You are given a string s. You need to reverse each word in it where the words are separated by spaces and return the modified string.
+
+// Note: The string may contain leading or trailing spaces, or multiple spaces between two words. The returned string should only have a single space separating the words, and no extra spaces should be included.
+
+// Code:
+class Solution {
+    public String reverseWords(String s) {
+        // remove leading/trailing + multiple spaces
+        String[] words = s.trim().split("\\s+");
+
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            result.append(new StringBuilder(word).reverse());
+            result.append(" ");
+        }
+
+        // remove last extra space
+        return result.toString().trim();
+
+    }
+}
+// Time Complexity: O(n), where n is the length of the string.
+// Space Complexity: O(n), since we are using a StringBuilder to store the result.
