dac070d659d4ac4dcaf855e88244b72e79f91911
Piyush Arora <piyusharora1504@gmail.com>
2026-03-03 21:43:18 +0530

    Check for Cycle in Linked List

--- LinkedList/LLCycle.java
+++ LinkedList/LLCycle.java
@@ -1,12 +1,31 @@
 // Problem Link: https://leetcode.com/problems/linked-list-cycle
 
-// Brute Force Approach: We can use a HashMap to store the visited nodes. If we encounter a node that is already in the HashMap, then there is a cycle in the linked list. The time complexity of this approach is O(n) and the space complexity is O(n).
+/*
 
-// Code:
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+ */
+
+ /*  Brute Force Approach: 
+
+We can use a HashMap to store the visited nodes. 
+If we encounter a node that is already in the HashMap, then there is a cycle in the linked list. 
+The time complexity of this approach is O(n) and the space complexity is O(n).
+
+Code:
+
+import java.util.HashMap;
 public class LLCycle {
     public boolean hasCycle(ListNode head) {
         ListNode temp = head;
-        Map<ListNode,Integer> nodeMap = new HashMap<>();
+        HashMap<ListNode,Integer> nodeMap = new HashMap<>();
         while(temp != null){
             if(nodeMap.containsKey(temp)){
                 return true;
@@ -18,17 +37,25 @@ public class LLCycle {
     }
 }
 
-// Optimal Approach: We can use two pointers, one slow and one fast. The slow pointer moves one step at a time while the fast pointer moves two steps at a time. If there is a cycle in the linked list, then the fast pointer will eventually meet the slow pointer. The time complexity of this approach is O(n) and the space complexity is O(1).
+ */
+ /*
 
+Optimal Approach: Fast and Slow Pointers (Floyd’s Tortoise and Hare Algorithm): 
+This is a common approach to detect a cycle in a linked list. The idea is to use two pointers, one slow and one fast. 
+The slow pointer moves one step at a time while the fast pointer moves two steps at a time. 
+If there is a cycle in the linked list, then the fast pointer will eventually meet the slow pointer. 
+The time complexity of this approach is O(n) and the space complexity is O(1).
+
+ */
 // Code:
-public class Solution {
+public class LLCycle {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == fast){
+            if (slow == fast) {
                 return true;
             }
         }
