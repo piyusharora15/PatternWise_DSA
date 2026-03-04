1f1022e22ce865f21b9f1aa01b7dcc934d97a70d
Piyush Arora <piyusharora1504@gmail.com>
2026-03-04 20:55:23 +0530

    Trapping Rainwater

--- Stack/TrappingRainwater.java
+++ Stack/TrappingRainwater.java
@@ -1,8 +1,31 @@
 // Problem Link : https://leetcode.com/problems/trapping-rain-water
 
-// Brute Force Approach: For each index, we have to find the amount of water that can be stored and we have to sum it up. If we observe carefully, the amount of water stored at a particular index is the minimum of the maximum elevation to the left and right of the index minus the elevation  at that index. So, we can iterate through each index and for each index, we can find the maximum elevation to the left and right of the index and then find the amount of water that can be stored at that index. The time complexity of this approach is O(n^2) and the space complexity is O(1).
+/*
 
-// Code :
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+
+Example 2:
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+*/
+
+/* 
+
+Brute Force Approach: 
+For each index, we have to find the amount of water that can be stored and we have to sum it up. 
+If we observe carefully, the amount of water stored at a particular index is the minimum of the maximum elevation to the left and right of the index minus the elevation  at that index. 
+So, we can iterate through each index and for each index, we can find the maximum elevation to the left and right of the index and then find the amount of water that can be stored at that index. 
+The time complexity of this approach is O(n^2) and the space complexity is O(1).
+
+*/
+
+/* Code :
+
 public int trap(int[] height) {
     int n = height.length;
     int waterTrapped = 0;
@@ -23,7 +46,17 @@ public int trap(int[] height) {
     return waterTrapped;
 }
 
-// Better Approach : We are taking O(n) time for computing leftMax and rightMax at each index. The complexity can be boiled down to O(1) if we precompute the leftMax and rightMax at each index. Take 2 arrays : prefix and suffix and precompute the leftMax and rightMax for each index beforehand. Then use the formula : min(prefix[i], suffix[i]) - height[i] to compute the water trapped at each index. The time complexity of this approach is O(n) and the space complexity is O(n).
+*/
+
+/* Better Approach : 
+
+We are taking O(n) time for computing leftMax and rightMax at each index. 
+The complexity can be boiled down to O(1) if we precompute the leftMax and rightMax at each index. 
+Take 2 arrays : prefix and suffix and precompute the leftMax and rightMax for each index beforehand. 
+Then use the formula : min(prefix[i], suffix[i]) - height[i] to compute the water trapped at each index. 
+The time complexity of this approach is O(n) and the space complexity is O(n).
+
+Code:
 
 class Solution {
     public int trap(int[] height) {
@@ -46,10 +79,20 @@ class Solution {
     }
 }
 
-// Optimal Aprroach : Two Pointer Approach.
-// Approach: Take 2 pointers l(left pointer) and r(right pointer) pointing to 0th and (n-1)th index respectively. Take two variables leftMax and rightMax and initialize them to 0. If height[l] is less than or equal to height[r] then if leftMax is less than height[l] update leftMax to height[l] else add leftMax-height[l] to your final answer and move the l pointer to the right i.e l++. If height[r] is less than height[l], then now we are dealing with the right block. If height[r] is greater than rightMax, then update rightMax to height[r] else add rightMax-height[r] to the final answer. Now move r to the left. Repeat these steps till l and r crosses each other.
+*/
+
+/* Optimal Aprroach : Two Pointer Approach.
 
 // Intuition: We need a minimum of leftMax and rightMax.So if we take the case when height[l]<=height[r] we increase l++, so we can surely say that there is a block with a height more than height[l] to the right of l. And for the same reason when height[r]<=height[l] we can surely say that there is a block to the left of r which is at least of height[r]. So by traversing these cases and using two pointers approach the time complexity can be decreased without using extra space.
+
+// Approach:
+Take 2 pointers l(left pointer) and r(right pointer) pointing to 0th and (n-1)th index respectively. 
+Take two variables leftMax and rightMax and initialize them to 0. 
+If height[l] is less than or equal to height[r] then if leftMax is less than height[l] update leftMax to height[l] else add leftMax-height[l] to your final answer and move the l pointer to the right i.e l++.
+If height[r] is less than height[l], then now we are dealing with the right block. If height[r] is greater than rightMax, then update rightMax to height[r] else add rightMax-height[r] to the final answer. Now move r to the left. 
+Repeat these steps till l and r crosses each other.
+
+*/
 
 // Code :
 class Solution {
