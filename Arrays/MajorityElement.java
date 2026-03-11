bf0196943a7cca8fce7c7515e10b71c8f38e4f2a
Piyush Arora <piyusharora1504@gmail.com>
2026-03-11 22:11:11 +0530

    Majority Element

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -1,10 +1,29 @@
 // Problem Link: https://leetcode.com/problems/majority-element
 
+/*
+
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+*/
+
 // Approach 1: HashMap to count occurrences.
 // We can use a HashMap to count the occurrences of each element in the array.
 // The element with the count greater than n/2 is the majority element.
 
 // Code:
+/*
+
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
@@ -21,14 +40,20 @@ class Solution {
     }
 }
 
+*/
+
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 
+
 // Approach 2: Boyer-Moore Voting Algorithm.
-// This algorithm works by maintaining a count and a candidate for the majority element. We increment the count when we see the candidate and decrement it when we see a different element. When the count reaches zero, we change the candidate to the current element. At the end, the candidate will be the majority element.
+// This algorithm works by maintaining a count and a candidate for the majority element. 
+// We increment the count when we see the candidate and decrement it when we see a different element. 
+// When the count reaches zero, we change the candidate to the current element. 
+// At the end, the candidate will be the majority element.
 
 // Code:
-class Solution {
+class MajorityElement {
     public int majorityElement(int[] nums) {
         int n = nums.length;
         int cnt = 0,ele = 0;
