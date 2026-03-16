500a50ea6d7eac25d6054cd450df1667a37e2603
Piyush Arora <piyusharora1504@gmail.com>
2026-03-16 21:51:11 +0530

    Group Anagrams

--- Strings/GroupAnagrams.java
+++ Strings/GroupAnagrams.java
@@ -1,5 +1,35 @@
 // Problem Link: https://leetcode.com/problems/group-anagrams?envType=problem-list-v2&envId=string
 
+/*
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+
+*/
+
 // Approach : Sorting each string and using it as a key in a hashmap to group anagrams together.
 // We sort each string and store it in a hashmap where the key is the sorted string and the value is a list of anagrams.
 
