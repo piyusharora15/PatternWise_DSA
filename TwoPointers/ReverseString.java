892235c352cacb7a9d4fcf428e1ab02e3a290cd1
Piyush Arora <piyusharora1504@gmail.com>
2026-03-17 21:10:08 +0530

    Reverse String

--- TwoPointers/ReverseString.java
+++ TwoPointers/ReverseString.java
@@ -1,10 +1,24 @@
 // Problem Link: https://leetcode.com/problems/reverse-string?envType=problem-list-v2&envId=string
 
-// Approach 1: Two Pointer In-Place Swap.
+/*
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+*/
+
+// Approach : Two Pointer In-Place Swap.
 // Place left at 0 and right at n-1. Swap s[left] and s[right], then left++, right--, repeat until left >= right. In-place, constant extra memory.
 
 // Code:
-class Solution {
+class ReverseString {
     public void reverseString(char[] s) {
         int left=0,right=s.length-1;
         while(left < right){
