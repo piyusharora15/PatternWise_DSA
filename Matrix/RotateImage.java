3672ef0457f11bb7dc35456e0fb5b72afec3b496
Piyush Arora <piyusharora1504@gmail.com>
2026-05-04 17:49:45 +0530

    Rotate Image problem

--- Matrix/RotateImage.java
+++ Matrix/RotateImage.java
@@ -1,34 +1,55 @@
 // Problem Link: https://leetcode.com/problems/rotate-image
 
-// Approach: First, we transpose the matrix by swapping elements across the diagonal.
-// Then, we reverse each row to achieve the 90-degree clockwise rotation.
+/*
 
-// Time Complexity: O(n^2), where n is the number of rows (or columns) in the matrix.
-// Space Complexity: O(1), as the rotation is done in place without using extra space
+Companies: Facebook, Microsoft, Amazon, Apple, Google.
+
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+Example 2:
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+
+Approach: 
+1. Transpose the matrix: Swap the elements at positions (i, j) and (j, i) for all i and j.
+2. Reverse each row: For each row, reverse the order of the elements.
+
+*/
+
 
 // Code:
-class Solution {
+class RotateImage {
+
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-       for(int i=0;i<n-1;i++)
-       {
-        for(int j=i+1;j<n;j++)
-        {
-            int temp = 0;
-            temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
+        // Transpose the matrix
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
         }
-       }
-       for(int i=0;i<n;i++)
-       {
-        for(int j=0;j<n/2;j++)
-        {
-            int temp = 0;
-            temp = matrix[i][j];
-            matrix[i][j] = matrix[i][n-1-j];
-            matrix[i][n-1-j] = temp;
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int start = 0, end = n - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
         }
-       }
     }
 }
+
+// Time Complexity: O(n^2) where n is the number of rows (or columns) in the matrix.
+// Space Complexity: O(1) since we are rotating the matrix in-place without using any additional data structures.
