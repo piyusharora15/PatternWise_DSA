7e0117ef1f5fde649ffe52c221222eb79c900ccb
Piyush Arora <piyusharora1504@gmail.com>
2026-05-14 20:28:28 +0530

    Find the Peak Index in a Mountain Array

--- Binary_Search/PeakIndexInAMountainArray.java
+++ Binary_Search/PeakIndexInAMountainArray.java
@@ -1,10 +1,39 @@
 // Problem Link: https://leetcode.com/problems/peak-index-in-a-mountain-array?envType=problem-list-v2&envId=wh88bf73
 
-// Approach: Binary Search.
-// We can use binary search to find the peak index in a mountain array. We will compare the middle element with its next element to determine whether we are in the increasing part of the mountain or the decreasing part. If the middle element is less than its next element, it means we are in the increasing part, and we need to move to the right half. Otherwise, we are in the decreasing part, and we need to move to the left half. We will continue this process until we find the peak index.
+/*
+
+Companies: Amazon, Microsoft, Google, Facebook.
+
+You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+
+Return the index of the peak element.
+
+Your task is to solve it in O(log(n)) time complexity.
+
+Example 1:
+Input: arr = [0,1,0]
+Output: 1
+
+Example 2:
+Input: arr = [0,2,1,0]
+Output: 1
+
+Example 3:
+Input: arr = [0,10,5,2]
+Output: 1
+
+Approach: Binary Search.
+
+We can use binary search to find the peak index in a mountain array. 
+We will compare the middle element with its next element to determine whether we are in the increasing part of the mountain or the decreasing part. 
+If the middle element is less than its next element, it means we are in the increasing part, and we need to move to the right half. 
+Otherwise, we are in the decreasing part, and we need to move to the left half. 
+We will continue this process until we find the peak index.
+
+*/
 
 // Code:
-class Solution {
+class PeakIndexInAMountainArray {
     public int peakIndexInMountainArray(int[] arr) {
         int left = 0;
         int right = arr.length - 1;
