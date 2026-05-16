e1c9a75b104b8bae52a6613a1a134e38fe77638c
Piyush Arora <piyusharora1504@gmail.com>
2026-05-16 21:01:29 +0530

    Find Minimum Element in a Rotated Sorted Array containing Duplicates

--- Binary_Search/FindMinimumInRotatedSortedArray2.java
+++ Binary_Search/FindMinimumInRotatedSortedArray2.java
@@ -1,7 +1,29 @@
 // Problem Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii
 
-// Optimal Approach: Modified Binary Search.
 /*
+
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
+
+[4,5,6,7,0,1,4] if it was rotated 4 times.
+[0,1,4,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
+
+You must decrease the overall operation steps as much as possible.
+
+Example 1:
+
+Input: nums = [1,3,5]
+Output: 1
+
+Example 2:
+
+Input: nums = [2,2,2,0,1]
+Output: 0
+
+Optimal Approach: Modified Binary Search.
+
 At each step compare nums[mid] with nums[right]:
 If nums[mid] > nums[right] → minimum is in (mid+1 .. right] → set left = mid + 1.
 Else if nums[mid] < nums[right] → minimum is in [left .. mid] → set right = mid.
@@ -9,10 +31,11 @@ Else (nums[mid] == nums[right]) → ambiguous because duplicates hide which side
 Stop when left == right; return nums[left].
 
 Why this is correct: If nums[mid] > nums[right] the rotation point is to the right; if nums[mid] < nums[right] the min is at mid or to the left. When equal, you cannot tell which side contains the pivot, but decrementing right preserves the minimum in the remaining window.
+
 */
 
 // Code:
-class Solution {
+class FindMinimumInRotatedSortedArray2 {
     public int findMin(int[] nums) {
         int l = 0,r = nums.length-1;
         while(l < r){
